2ca2e8b55dff7474e13b4e76b75d38251e4a54af
Антонов Андрей <an_42@mail.ru>
2020-09-12 12:37:19 +0700

    разобрался с webpack.mix: как подмешивать файлы и запускать npm run watch; по человечески интегрировал Vue (отдельно один экран, не все вьюхи);

--- exercise3/webpack.mix.js
+++ exercise3/webpack.mix.js
@@ -12,4 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+  .styles('resources/css/style.css', 'public/css/style.css')
+  .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles('resources/css/start.css', 'public/css/start.css');
+
+mix.styles('resources/css/login.css', 'public/css/login.css');
+
+mix.js('resources/js/home.js', 'public/js/home.js')
+  .styles('resources/css/home.css', 'public/css/home.css');
